511dc04451b559e35aa2ed58bab66d0aeea5b2f4
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-01-26 23:43:23 +0700

    fix: eslint error и поправил formatter.js

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -7,26 +7,20 @@ export function formatDate(date){
 
 export function getDuration(startDate, endDate) {
   const diffMinutes = dayjs(endDate).diff(startDate, 'minute');
-
   const days = Math.floor(diffMinutes / (24 * 60));
   const restMinutesAfterDays = diffMinutes % (24 * 60);
-
   const hours = Math.floor(restMinutesAfterDays / 60);
   const minutes = restMinutesAfterDays % 60;
+
+  const formatPart = (value) => String(value).padStart(2, '0');
 
   if (diffMinutes < 60) {
     return `${minutes}M`;
   }
 
   if (diffMinutes < 24 * 60) {
-    const hoursStr = String(hours).padStart(2, '0');
-    const minutesStr = String(minutes).padStart(2, '0');
-    return `${hoursStr}H ${minutesStr}M`;
+    return `${formatPart(hours)}H ${formatPart(minutes)}M`;
   }
 
-  const daysStr = String(days).padStart(2, '0');
-  const hoursStr = String(hours).padStart(2, '0');
-  const minutesStr = String(minutes).padStart(2, '0');
-
-  return `${daysStr}D ${hoursStr}H ${minutesStr}M`;
+  return `${formatPart(days)}D ${formatPart(hours)}H ${formatPart(minutes)}M`;
 }
